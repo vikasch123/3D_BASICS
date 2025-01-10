c6d51556ddd7f1b56f7d1df6b24fde6574f99c97
vikasch123 <kvikaskumar004@gmail.com>
2025-01-10 20:49:06 +0530

    Added geometry,light,camera,scene and boring animation

--- index.js
+++ index.js
@@ -1,22 +1,34 @@
 import * as THREE from 'three';
 
 const scene = new THREE.Scene();
+scene.background = new THREE.Color('#000000');
+
+// Add a Camera
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-const renderer = new THREE.WebGLRenderer({
-  canvas: document.getElementById('canvas'),
-  antialias: true
-});
+camera.position.z=5;
+
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const material = new THREE.MeshLambertMaterial({ color: '468585',emissive: '#468585' });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
+
+//Add light
+const light = new THREE.DirectionalLight(0x9CDBA6, 10);
+light.position.set(1,1,1);   //set(x,y,z)
 
 camera.position.z = 5;
 camera.lookAt(scene.position);
 
-function animate() {
-  requestAnimationFrame(animate);
-  renderer.render(scene, camera);
-}
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+ function animate(){
+   requestAnimationFrame(animate);
+   cube.rotation.x += 0.01;
+   cube.rotation.y += 0.01;
+   renderer.render(scene, camera);
+ }
+
 animate();
